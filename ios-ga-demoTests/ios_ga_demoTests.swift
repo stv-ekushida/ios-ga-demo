edc3ead66968b873cef8c1e98977c665404f08dc
Eiji Kushida <ekushida@EijinoMacBook-Pro.local>
2017-01-17 14:16:49 +0900

    Initial Commit

---
+++ ios-ga-demoTests/ios_ga_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_ga_demoTests.swift
+//  ios-ga-demoTests
+//
+//  Created by Eiji Kushida on 2017/01/17.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import XCTest
+@testable import ios_ga_demo
+
+class ios_ga_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
